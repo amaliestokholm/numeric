ddefd6ec48556a23f91dd218e2675494c90f7187
Amalie Stokholm <stokholm@post.au.dk>
2017-05-25 18:19:31 +0200

    Cubic spline works!

---
+++ 1_interpolation/cmain.py
@@ -0,0 +1,29 @@
+import numpy as np
+from interp import interpolation as interp
+
+# Define start and end point and the number of points in the function (n) and in the interpolation (N)
+n = 50
+N = 100
+start = 0
+end = 4 * np.pi
+
+# Make input -- as an example, the cosine function is used.
+x = np.linspace(start, end, n)
+y = np.cos(x)
+z = np.linspace(start, end, N)
+y_dev = - np.sin(x)
+y_int = np.sin(x)
+
+# Initialize the class
+i_cos = interp(x, y)
+
+# Interpolate
+s_cspl, s_csplint, s_cspldev = i_cos.cspline(z)
+
+# Print the values in order to save the stdout
+for i in np.arange(N):
+    if i < n:
+        print('%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s'
+              % (z[i], s_cspl[i], s_csplint[i], s_cspldev[i], x[i], y[i], y_int[i], y_dev[i]))
+    else:
+        print('%s\t%s\t%s\t%s' % (z[i], s_cspl[i], s_csplint[i], s_cspldev[i]))
--- 1_interpolation/interp.py
+++ 1_interpolation/interp.py
@@ -70,6 +70,59 @@ class interpolation:
         return b, c
 
 
+    def __cspline_params(self):
+        """
+        Calculate the coefficients b, c, and d for cubic spline
+        """
+        b = np.zeros(self.n)
+        c = np.zeros(self.n-1)
+        d = np.zeros(self.n-1)
+        B = np.zeros(self.n)
+        Q = np.ones(self.n-1)
+        D = 2 * np.ones(self.n)
+        dx = np.zeros(self.n-1)
+        p = np.zeros(self.n-1)
+
+        # Calculate x-interval and slope
+        for j in np.arange(self.n-1):
+            dx[j] = self.x[j+1] - self.x[j]
+            p[j] = (self.y[j+1] - self.y[j]) / dx[j]
+
+        # Fill B
+        B[0] = 3 * p[0]
+        for i in np.arange(self.n-2):
+            B[i+1] = 3 * (p[i] + p[i+1] * dx[i] / dx[i+1])
+        B[-1] = 3 * p[-2]
+        
+        # Fill D
+        for i in np.arange(self.n-2):
+            D[i+1] = 2 * dx[i] / dx[i+1] + 2
+
+        # Fill Q
+        for i in np.arange(self.n-2):
+            Q[i+1] = dx[i] / dx[i+1]
+
+        # Gauss elimination
+        for i in np.arange(1, self.n):
+            D[i] = D[i] - Q[i-1] / D[i-1]
+            B[i] = B[i] - B[i-1] / D[i-1]
+
+        # Back-substitution
+        b[-1] = B[-1] / D[-1]
+        list = np.arange(self.n-1)
+        for i in list[::-1]:
+            b[i] = (B[i] - Q[i] * b[i+1]) / D[i]
+
+        # Calculate c and d
+        for i in np.arange(self.n-1):
+            c[i] = (3 * p[i] - 2 * b[i] - b[i+1]) / dx[i]
+            d[i] = (b[i] + b[i+1] - 2 * p[i]) / dx[i]
+        c[-1] = -3 * d[-1] * dx[-1]
+
+        return b, c, d
+
+
+
     def __linterp_integ(self, z):
         """
         Linear interpolation integral
@@ -93,11 +146,26 @@ class interpolation:
         b, c = self.__qspline_params()
         for j in np.arange(i):
             dx = self.x[j+1] - self.x[j]
-            result += self.y[j] * dx + 0.5 * b[j] * dx ** 2 + (1 / 3) * c[j] * dx ** 2
+            result += (self.y[j] * dx + 0.5 * b[j] * dx ** 2 + (1 / 3) * c[j] * dx ** 2)
         zi = z - self.x[i]
         result +=  self.y[i] * zi + 0.5 * b[i] * zi ** 2 + (1 / 3) * c[i] * zi ** 2
         return result
 
+
+    def __cspline_integ(self, x):
+        """
+        Cubic spline integral
+        """
+        result = 0
+        p, i = self.__binarysearch(z)
+        b, c = self.__qspline_params()
+        for j in np.arange(i):
+            dx = self.x[j+1] - self.x[j]
+            result += (self.y[j] * dx + 0.5 * b[j] * dx ** 2 + (1 / 3) * c[j] * dx ** 2 
+                       + (1 / 4) * d[j] * dx ** 3)
+        zi = z - self.x[i]
+        result +=  self.y[i] * zi + 0.5 * b[i] * zi ** 2 + (1 / 3) * c[i] * zi ** 2 + (1 / 4) * d[i] * zi ** 3
+        return result
 
     def linterp(self, z):
         """
@@ -132,3 +200,23 @@ class interpolation:
                 sd[j] = b[i] + 2 * c[i] * (z[j] - self.x[i])
         return s, si, sd
 
+
+    def cspline(self, z, deriv_flag=1, int_flag=1, func_flag=1):
+        """
+        Cubic spline interpolation routine
+        """
+        z = np.asarray(z)
+        s = np.zeros(z.shape)
+        si = np.zeros(z.shape)
+        sd = np.zeros(z.shape)
+        b, c, d = self.__cspline_params()
+        for j in np.arange(z.size):
+            p, i = self.__binarysearch(z[j])
+            if func_flag is not None:
+                s[j] = (self.y[i] + b[i] * (z[j] - self.x[i]) + c[i] * (z[j] - self.x[i]) ** 2 
+                        + d[i] * (z[j] - self.x[i]) ** 3) 
+            if int_flag is not None:
+                si[j] = self.__qspline_integ(z[j])
+            if deriv_flag is not None:
+                sd[j] = b[i] + 2 * c[i] * (z[j] - self.x[i]) + 3 * d[i] * (z[j] - self.x[i]) ** 2
+        return s, si, sd
