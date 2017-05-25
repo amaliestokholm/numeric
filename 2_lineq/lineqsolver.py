f0c29d6c95b782b45dfd91da1849138af5f02496
Amalie Stokholm <stokholm@post.au.dk>
2017-05-25 21:56:23 +0200

    Start linear equation solver

--- 2_lineq/lineqsolver.py
+++ 2_lineq/lineqsolver.py
@@ -1,6 +1,5 @@
 import numpy as np
 
-
 # Define dimensions of matrices
 n = 3
 m = 3
@@ -8,7 +7,7 @@ assert n >= m
 
 def qr_gs_decomp(A, R):
     """
-    This function performs an in-place modified GRam-Schmidt orthogonalization of an nxm matrix A.
+    This function performs an in-place modified Gram-Schmidt orthogonalization of an nxm matrix A.
     A turns into Q and the square mxm matrix R is computed.
     """
     n, m = A.shape
@@ -17,7 +16,18 @@ def qr_gs_decomp(A, R):
     assert p == q
     assert m == p
 
+    Q = np.zeros((m, m),)
+
+    # Gram-Schmidt orthogonalization
     for i in np.arange(m):
+        R[i, i] = np.sqrt(np.dot(A[:, i], A[:, i]))
+        Q[:, i] = A[:, i] / R[i, i]
+        for j in np.arange(i+1, m):
+            R[i, j] = np.dot(Q[:, i], A[:, j])
+            A[:, j] = A[:, j] - Q[:, i] * R[i, j]
 
 
+A = np.random.rand(n, m)
+R = np.random.rand(m, m)
 
+qr_gs_decomp(A, R)
