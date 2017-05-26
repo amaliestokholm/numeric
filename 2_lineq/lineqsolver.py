ba1a04686e093b591575221fdffc6e2de0f3fba0
Amalie Stokholm <stokholm@post.au.dk>
2017-05-26 21:20:10 +0200

    Check of inverse matrix using Givens rotation does not work

--- 2_lineq/lineqsolver.py
+++ 2_lineq/lineqsolver.py
@@ -54,3 +54,47 @@ def qr_gs_inverse(Q, R, b):
     for i in np.arange(m):
         b[:, i] = eye[:, i]
         qr_gs_solve(Q, R, b[:, i])
+
+
+def qr_gv_decomp(A):
+    """
+    This function decomposes a matrix A using the Givens rotations
+    """
+    n, m = A.shape
+    assert n >= m
+    for p in np.arange(m):
+        for q in np.arange(p+1, n):
+            theta = np.arctan2(A[q, p], A[p, p])
+            for i in np.arange(p, m):
+                xp = A[p, i]
+                xq = A[q, i]
+                A[p, i] = xp * np.cos(theta) + xq * np.sin(theta)
+                A[q, i] = -xp * np.sin(theta) + xq * np.cos(theta)
+            # Store the rotation angles in the corresponding place in matrix A
+            A[q, p] = theta
+
+
+def qr_gv_solve(A, b):
+    """
+    This function solves Ax = b if A has been decomposed
+    """
+    n, m = A.shape
+    for p in np.arange(m):
+        for q in np.arange(p+1, n):
+            theta = A[q, p]
+            xp = np.copy(b[p])
+            xq = np.copy(b[q])
+            b[p] = xp * np.cos(theta) + xq * np.sin(theta)
+            b[q] = - xp * np.sin(theta) + xq * np.cos(theta)
+    backsubstitution(A, b)
+
+
+def qr_gv_inverse(A, b):
+    """
+    This function calculates the inverse of A into b
+    """
+    n, m = A.shape
+    assert n == m
+    b = np.identity(m)
+    for i in np.arange(m):
+        b[:, i] = qr_gv_solve(A, b[:, i])
--- 2_lineq/main.py
+++ 2_lineq/main.py
@@ -1,5 +1,5 @@
 import numpy as np
-from lineqsolver import qr_gs_decomp, qr_gs_solve, qr_gs_inverse
+from lineqsolver import qr_gs_decomp, qr_gs_solve, qr_gs_inverse, qr_gv_decomp, qr_gv_solve, qr_gv_inverse
 
 """
 CHECKS
@@ -14,7 +14,7 @@ m = 3
 rtol = 1e-16
 
 A = np.random.rand(n, m)
-A_check = A.copy() 
+A_check = np.copy(A) 
 R = np.random.rand(m, m)
 
 print('Given the matrix A with dimensions {}x{}, where A in this run is\n{}'.format(*A.shape, A))
@@ -28,14 +28,15 @@ print('We check if R is upper triangular:\n{}'.format(R))
 QR = np.dot(A, R)
 print('Are QR equal to A within a tolerance of {}? \n{}'.format(A, R, QR, rtol, np.allclose(QR, A_check, rtol)))
 
-n = 4
-A = np.random.rand(n, n)
-A_check = A.copy()
-R = np.random.rand(n, n)
-b = np.random.rand(n)
-b_check = b.copy()
 
 print('\nCheck part A.2')
+n = 4
+A = np.random.rand(n, n)
+A_check = np.copy(A)
+R = np.random.rand(n, n)
+b = np.random.rand(n)
+b_check = np.copy(b)
+
 print('Given the square matrix A with dimensions {}x{}, where A in this run is\n{}'.format(*A.shape, A))
 print('and given a random vector b of size {}, which in this run is\n{}'.format(*b.shape, b))
 qr_gs_decomp(A=A, R=R)
@@ -44,16 +45,44 @@ print('Using qr_gs_decomp on A and then using back-substitution, yields the solu
 Ax = np.dot(A_check, b)
 print('Is Ax = b within a tolerance of {}?\n {}'.format(rtol, np.allclose(Ax, b_check, rtol)))
 
+
 print('\nCheck part B')
 A = np.random.rand(m, m)
-A_check = A.copy()
+A_check = np.copy(A)
 R = np.random.rand(m, m)
 b = np.random.rand(m, m)
 print('Given the square matrix A of size {}x{}\n {}'.format(*A.shape, A))
 print('We calculate the inverse of A by GS-decomposing it')
 qr_gs_decomp(A=A, R=R)
 qr_gs_inverse(Q=A, R=R, b=b)
-print(b)
 AAi = np.dot(A_check, b) 
-print(AAi)
 print('Is the product of A and A^(-1) equal to I within a tolerance of {}?\n{}'.format(rtol, np.allclose(AAi, np.identity(m))))
+
+
+print('\nCheck part C')
+# Define the dimensions
+n = 5
+
+A = np.random.rand(n, n)
+Ai = np.random.rand(n, n)
+A_gv = np.copy(A) 
+A_check = np.copy(A) 
+R = np.random.rand(n, n)
+b = np.random.rand(n)
+b_check = np.copy(b)
+
+print('Given the matrix A with dimensions {}x{}, where A in this run is\n{}'.format(*A.shape, A))
+print('We try to solve the equationUsing Gram-Schmidt as earlier')
+qr_gs_decomp(A=A, R=R)
+qr_gs_solve(Q=A, R=R, b=b)
+print('After the run of qr_qs_decomp, b is \n{}'.format(b))
+qr_gv_decomp(A=A_gv)
+qr_gv_solve(A=A_gv, b=b_check)
+print('If we try to solve the equation using Givens-rotations, we get \n{}'.format(b_check)) 
+print('Are the two solutions equal? {}'.format(np.allclose(b, b_check, rtol))) 
+qr_gv_inverse(A_gv, Ai)
+AAi = np.dot(A_check, A_gv)
+eye = np.identity(n)
+print(AAi)
+print('We calculate the inverse and checks if AA^(-1) = I. Are AA^(-1)=I within the tolerance {}?\n {}'.format(rtol, np.allclose(AAi, eye, rtol)))
+
