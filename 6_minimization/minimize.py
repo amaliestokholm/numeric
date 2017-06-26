e961920d31d055ee371b9d5325d071ef84da6523
Amalie Stokholm <stokholm@post.au.dk>
2017-06-26 20:44:56 +0200

    Implement qnewton more or less

--- 6_minimization/minimize.py
+++ 6_minimization/minimize.py
@@ -8,7 +8,7 @@ from lineqsolver import qr_gv_solve as solve
 import globvar
 
 
-def newton_minimize(f, grad, hessian, x0, alpha, eps=1e-10):
+def newton_minimize(f, grad, hessian, x0, alpha=1e-4, eps=1e-10):
     """
     This routine finds the minimum of a function using Newton's method.
     The derivatives (gradiant and hessian) are supplied by the user.
@@ -43,7 +43,7 @@ def newton_minimize(f, grad, hessian, x0, alpha, eps=1e-10):
         Dx = -df
         solve(H, Dx)
 
-        # Begin backtracking linesearch
+        # Begin linear backtracking linesearch
         lamb = 2.0
         dfDx = np.dot(df, Dx)
 
@@ -72,7 +72,74 @@ def newton_minimize(f, grad, hessian, x0, alpha, eps=1e-10):
         return x
 
 
-def qnewton(f, grad, x0, alpha, eps=1e-10):
+def qnewton(f, grad, x0, alpha=1e-4, eps=1e-10):
     """
-    jf½
+    This routine finds the minimum of a function using Quasi-Newton's method.
+    The gradient is supplied by the user.
+    Arguments:
+        - 'f': Function f(x) to root-find, which takes x as a vector.
+        - 'grad': Function which calculates the gradient of f
+        - 'x0': Vector containing starting point.
+        - 'dx': Vector containing the slope used in evalution of the Jacobian
+        - 'alpha': Scaling factor
+        - 'eps': Desired tolerance.
+    Returns:
+        - 'x': Approximated root
     """
+    # Initialization
+    globvar.steps = 0
+    x = np.copy(x0)
+    n = len(x)
+    fx = f(x)
+    df = grad(x)
+
+    # Calculate the inverse of the Hessian matrix as I
+    Hinv = np.identity(n)
+
+    # Begin root search
+    while True:
+        globvar.steps += 1
+
+        # Put derivatives into the inverse Hessian matrix
+        Dx = np.dot(Hinv, -df)
+
+        # Begin linear backtracking linesearch
+        lamb = 2.0
+        dfDx = np.dot(df, Dx)
+
+        while True: 
+            lamb /= 2
+            y = x + Dx * lamb
+            fy = f(y)
+
+            # The Armijo condition
+            if (fy < fx + alpha * lamb * dfDx)
+                break
+
+            # Reset if the update diverges
+            if (lamb < (1 / 128.0)):
+                Hinv = np.identity(n)
+                break
+
+        # Apply the update
+        dfy = grad(y)
+        z = dfy - df
+        u = Dx * lamb - np.dot(Hinv, z)
+
+        # SR1-update
+        Hinv += np.outer(u, u) / np.dot(u, z)
+
+        # Save latest approximation
+        x = y
+        fx = fy
+        df = grad(x)
+
+        dfnorm = np.linalg.norm(df)
+        if (dfnorm < eps) or (globvar.steps > stepmax):
+            break
+
+    if globvar.steps > stepmax:
+        print('\nToo many steps used!')
+        exit()
+    else:
+        return x
