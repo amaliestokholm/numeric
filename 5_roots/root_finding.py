f8a3ea51162c4a20b567a0fad7860b51fc8f53e3
Amalie Stokholm <stokholm@post.au.dk>
2017-06-26 13:05:53 +0200

    Write all Jacobian functions

--- 5_roots/root_finding.py
+++ 5_roots/root_finding.py
@@ -68,3 +68,58 @@ def newton(f, x0, dx, eps=1e-10):
             break
 
     return x
+
+
+def newton_jacobian(f, x0, Jf, eps=1e-10):
+    """
+    This routine finds the root of a function using Newton's method
+    using a user-supplied Jacobian.
+    It uses Given's rotation for QR_decomposition.
+    Arguments:
+        - 'f': Function f(x) to root-find, which takes x as a vector.
+        - 'x0': Vector containing starting point.
+        - 'Jf': Function to calculate the Jacobian of f. 
+        - 'eps': Desired tolerance.
+    Returns:
+        - 'x': Approximated root
+    """
+    # Initialization
+    x = np.copy(x0)
+    n = len(x)
+    J =  np.zeros((n, n), dtype='float64')
+    fx = f(x)
+
+
+    # Begin root search
+    while True:
+
+        # Calculate Jacobian
+        J = Jf(x)
+
+        # Decompose and solve using Given's rotations
+        decomp(J)
+        Dx = -fx
+        solve(J, Dx)
+
+        # Begin backtracking linesearch
+        lamb = 2.0
+        while True:
+            lamb /= 2
+            y = x + Dx * lamb
+            fy = f(y)
+
+            fynorm = np.linalg.norm(fy)
+            fxnorm = np.linalg.norm(fx)
+            if fynorm < (1- lamb / 2) * fxnorm or (lamb < (1 / 128.0)):
+                break
+        # Save latest approximation
+        x = y
+        fx = fy
+
+        Dxnorm = np.linalg.norm(Dx)
+        fxnorm = np.linalg.norm(fx)
+        dxnorm = np.linalg.norm(dx)
+        if Dxnorm < dxnorm or fxnorm < eps:
+            break
+
+    return x
--- 5_roots/systems.py
+++ 5_roots/systems.py
@@ -25,6 +25,24 @@ def sys(q):
     return z
 
 
+def jacobian_sys(q):
+    """
+    Derivatives of sys in form of the Jacobian
+    """
+    # Initialize
+    J = np.zeros((2, 2), dtype='float64')
+    x = q[0]
+    y = q[1]
+    A = 10000.0
+
+    # Fill the Jacobian
+    J[0, 0] = A * y
+    J[0, 1] = A * x
+    J[1, 0] = -np.exp(-x)
+    J[1, 1] = -np.exp(-y)
+    return J
+
+
 def rosenbrock(q):
     """
     The Rosenbrock's valley function (or the gradient of it)
@@ -41,6 +59,23 @@ def rosenbrock(q):
     z[0] = 2 * (x - 1) - 400 * (y - x * x) * x
     z[1] = 200 * (y - x * x)
     return z
+
+
+def jacobian_rosenbrock(q):
+    """
+    Derivatives of Rosenbrock's valley function in form of the Jacobian
+    """
+    # Initialize
+    J = np.zeros((2, 2), dtype='float64')
+    x = q[0]
+    y = q[1]
+
+    # Fill the Jacobian
+    J[0, 0] = 2 - 400 * y + 1200 * x * x
+    J[0, 1] = -400 * x
+    J[1, 0] = -400 * x
+    J[1, 1] = 200
+    return J
 
 
 def himmelblau(q):
@@ -61,6 +96,23 @@ def himmelblau(q):
     return z
 
 
+def jacobian_himmelblau(q):
+    """
+    Derivatives of Himmelblau function in form of the Jacobian
+    """
+    # Initialize
+    J = np.zeros((2, 2), dtype='float64')
+    x = q[0]
+    y = q[1]
+
+    # Fill the Jacobian
+    J[0, 0] = 12 * x * x + 4 * y - 42 
+    J[0, 1] = 4 * x + 4 * y
+    J[1, 0] = 4 * x + 4 * y
+    J[1, 1] = 4 * x + 12 * y *y - 26
+    return J
+
+
 def matya(q):
     """
     Matyas function 
@@ -77,3 +129,20 @@ def matya(q):
     z[0] = 2 * 0.26 * x - 0.48 * y 
     z[1] = 2 * 0.26 * y - 0.48 * x
     return z
+
+
+def jacobian_matya(q):
+    """
+    Derivatives of Matya function in form of the Jacobian
+    """
+    # Initialize
+    J = np.zeros((2, 2), dtype='float64')
+    x = q[0]
+    y = q[1]
+
+    # Fill the Jacobian
+    J[0, 0] = 0.52
+    J[0, 1] = -0.48 
+    J[1, 0] =  -0.48
+    J[1, 1] = 0.52 
+    return J
