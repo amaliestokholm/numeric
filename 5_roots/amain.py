9a88c74bf43d36585c97cb4f463f17a0cd133dfe
Amalie Stokholm <stokholm@post.au.dk>
2017-06-26 10:30:41 +0200

    Part A done

--- 5_roots/amain.py
+++ 5_roots/amain.py
@@ -21,6 +21,36 @@ def amain():
     roots = root_finding.newton(systems.sys, x0, dx)
     print(roots)
     print('Number of calls:', globvar.ncalls)
+    print('\n\n')
 
+    # Initialization
     globvar.ncalls = 0
+    x0 = np.array([1, 400], dtype='float64')
+    dx = np.array([1e-9, 1e-9], dtype='float64')
+
+    print('Check part B')
+    print('Solve the Rosenbrock valley function')
+    print('Starting point: x0 =\n', x0)
+    print('f(x0) =', systems.rosenbrock(x0))
+    print('The roots using Newton method:')
+    roots = root_finding.newton(systems.rosenbrock, x0, dx)
+    print(roots)
+    print('Number of calls:', globvar.ncalls)
+    print('\n\n')
+
+    # Initialization
+    globvar.ncalls = 0
+    x0 = np.array([1, -1], dtype='float64')
+    dx = np.array([1e-9, 1e-9], dtype='float64')
+
+    print('Check part C')
+    print('Solve the Himmelblau function')
+    print('Starting point: x0 =\n', x0)
+    print('f(x0) =', systems.himmelblau(x0))
+    print('The roots using Newton method:')
+    roots = root_finding.newton(systems.himmelblau, x0, dx)
+    print(roots)
+    print('Number of calls:', globvar.ncalls)
+    print('\n\n')
+
 amain()
--- 5_roots/systems.py
+++ 5_roots/systems.py
@@ -1,6 +1,7 @@
 import numpy as np
 import globvar
 
+
 def sys(q):
     """
     System of equations with A = 10000.
@@ -22,3 +23,40 @@ def sys(q):
     z[0] = A * x * y - 1
     z[1] = np.exp(-x) + np.exp(-y) - 1 - (1 / A)
     return z
+
+
+def rosenbrock(q):
+    """
+    The Rosenbrock's valley function (or the gradient of it)
+    """
+    # Count the number of calls to function
+    globvar.ncalls += 1
+
+    # Initialize
+    z = np.zeros(2, dtype='float64')
+    x = q[0]
+    y = q[1]
+    
+    # Calculate
+    z[0] = 2 * (x - 1) - 400 * (y - x * x) * x
+    z[1] = 200 * (y - x * x)
+    return z
+
+
+def himmelblau(q):
+    """
+    The Himmelblau function (or the gradient of it)
+    """
+    # Count the number of calls to function
+    globvar.ncalls += 1
+
+    # Initialize
+    z = np.zeros(2, dtype='float64')
+    x = q[0]
+    y = q[1]
+    
+    # Calculate
+    z[0] = 4 * (x * x + y - 11) * x + 2 * (x + y * y - 7) 
+    z[1] = 2 * (x * x + y - 11) + 4 * (x + y * y -7) * y
+    return z
+
