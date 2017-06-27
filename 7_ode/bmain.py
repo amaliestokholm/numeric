fd776175306976f5eca9d5019128ebf2fb9f4965
Amalie Stokholm <stokholm@post.au.dk>
2017-06-27 21:44:55 +0200

    Non-embedded RK stepper seems to work

---
+++ 7_ode/bmain.py
@@ -0,0 +1,52 @@
+import numpy as np
+import ode
+import globvar
+
+
+def f(x, y):
+    """
+    ODE where y'' = -y, so trigometric function
+    """
+    globvar.ncalls += 1
+    return np.array([y[1], -y[0]])
+
+
+def bmain():
+    """
+    Test of the ODE routines
+    """
+    # Initialization
+    a = 0
+    b = np.pi ** 2
+    acc = 1e-3
+    eps = 1e-3
+    step = 0.1
+    yinit = np.array([0, 1], dtype='float64')
+    globvar.ncalls = 0
+
+    # Evolve the system
+    xs, ys, cs = ode.rkdriver(f, a, b, yinit, step, 'rkstep23', acc, eps,
+                              calls=True)
+    
+    print('Output from RK23: y0')
+    # Print output
+    for i in range(len(xs)):
+        print(xs[i], ys[i, 0], cs[i], sep='\t')
+    print('Output from RK23: y1')
+    for i in range(len(xs)):
+        print(xs[i], ys[i, 1], cs[i], sep='\t')
+
+    globvar.ncalls = 0
+    # Evolve the system
+    xs, ys, cs = ode.rkdriver(f, a, b, yinit, step, 'rkstep3', acc, eps,
+                              calls=True)
+    
+    print('Output from RK3: y0')
+    # Print output
+    for i in range(len(xs)):
+        print(xs[i], ys[i, 0], cs[i], sep='\t')
+    print('Output from RK3: y1')
+    for i in range(len(xs)):
+        print(xs[i], ys[i, 1], cs[i], sep='\t')
+
+bmain()
--- 7_ode/ode.py
+++ 7_ode/ode.py
@@ -1,4 +1,5 @@
 import numpy as np
+import globvar
 
 
 def rkstep23(F, x, y, h):
@@ -48,13 +49,73 @@ def rkstep23(F, x, y, h):
     # Approximate next step and error
     yh = y + b1 * k1 + b2 * k2 + b3 * k3 + b4 * k4
     yhs = y + bs1 * k1 + bs2 * k2 + bs3 * k3 + bs4 * k4
-    err = yh- yhs
+    err = yh - yhs
     errnorm = np.linalg.norm(err)
 
     return yh, errnorm
 
 
-def rkdriver(F, a, b, ya, h, method, acc=1e-9, eps=1e-9):
+def rkstep3(F, x, y, h):
+    """
+    Non-embedded Runge-Kutta stepper of order 3, using Runge's error
+    estimate.
+    Arguments:
+        - 'F': Function containing the right-hand side
+        - 'x': Position
+        - 'y': Value of function
+        - 'h': Step-size
+    Returns:
+        - 'yh': Estimate of function
+        - 'errnorm': Norm of error on the step
+    """
+    # Arrange data in Butchers tableau
+    c1 = 0
+    c2 = 1 / 2.0
+    c3 = 3.0 / 4.0
+    c4 = 1
+
+    # Make Runge-Kutta matrix
+    a21 = 1 / 2.0
+    a31 = 0
+    a32 = 3 / 4.0
+    a41 = 2 / 9.0
+    a42 = 1 / 3.0
+    a43 = 4 / 9.0
+
+    # Determine the weights
+    b1 = 2 / 9.0
+    b2 = 1 / 3.0
+    b3 = 4 / 9.0
+    b4 = 0
+
+    # Initialization
+    yl = np.copy(y)
+
+    # Estimate the error from full_step and two_half_steps
+    for l in range(3):
+        if l == 1:
+            yfull = np.copy(yh)
+            h /= 2
+        if l == 2:
+            yl = np.copy(yh)
+            x += h
+
+        # Calculate ks
+        k1 = h * F(x + c1 * h, yl)
+        k2 = h * F(x + c2 * h, yl + a21 * k1)
+        k3 = h * F(x + c3 * h, yl + a31 * k1 + a32 * k2)
+        k4 = h * F(x + c4 * h, yl + a41 * k1 + a42 * k2 + a43 * k3)
+
+        # Approximate next step and error
+        yh = yl + b1 * k1 + b2 * k2 + b3 * k3 + b4 * k4
+
+    err = (yfull - yh) / 7  # 7 = 2^p - 1, p = 3 Eq. [11]
+    errnorm = np.linalg.norm(err)
+
+    return yfull, errnorm
+
+
+def rkdriver(F, a, b, ya, h, method, acc=1e-9, eps=1e-9, calls=False):
     """
     This function is an adaptive step-size driver routine, which advances
     the solution from a given value.
@@ -74,6 +135,8 @@ def rkdriver(F, a, b, ya, h, method, acc=1e-9, eps=1e-9):
     # Identify which stepper to use
     if method.lower() in ['rkstep23', 'rk23']:
         stepper = rkstep23
+    elif method.lower() in ['rkstep3', 'rk3']:
+        stepper = rkstep3
     else:
         print('Unknown stepper chosen!')
         return
@@ -81,6 +144,7 @@ def rkdriver(F, a, b, ya, h, method, acc=1e-9, eps=1e-9):
     # Initialize
     xs = np.array([a], dtype='float64')
     ys = np.array([ya], dtype='float64')
+    cs = np.array([globvar.ncalls], dtype='float64')
     power = 0.25
     safety = 0.95
 
@@ -88,7 +152,7 @@ def rkdriver(F, a, b, ya, h, method, acc=1e-9, eps=1e-9):
     while True:
         x = xs[-1]
         y = ys[-1]
-        
+
         # Stopping criteria
         if x >= b:
             break
@@ -107,10 +171,16 @@ def rkdriver(F, a, b, ya, h, method, acc=1e-9, eps=1e-9):
         if err < tol:
             xs = np.append(xs, x + h)
             ys = np.vstack([ys, yh])
+            cs = np.append(cs, globvar.ncalls)
 
         # Decrease the step if error is non-zero, else double it.
         if err > 0:
             h *= (tol / err) ** (power) * safety
         else:
             h *= 2
-    return xs, ys
+
+    if (calls == True):
+        return xs, ys, cs
+
+    else:
+        return xs, ys
