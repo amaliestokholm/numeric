4fc673f1c3b5b56e7b664fe911e5121327a3643c
Amalie Stokholm <stokholm@post.au.dk>
2017-06-28 22:14:18 +0200

    Try to fix infinite integrals

--- 8_integ/integ.py
+++ 8_integ/integ.py
@@ -9,37 +9,38 @@ def nodes(a, b, x):
     return a + x * (b - a)
 
 
-def integ_recursive(F, a, b, acc=1e-4, eps=1e-4):
+def integ_recursive(F, a_orig, b_orig, acc=1e-4, eps=1e-4):
     """
     This routine computes recursive adaptive integration using open
     QUADratures. It uses a 4th order trapezium evaluation rule with
     2nd order rectangular rule for error estimation.
     Arguments:
         - 'F': Function to be integrated
-        - 'a': Starting point
-        - 'b': End point
+        - 'a_orig': Starting point
+        - 'b_orig': End point
         - 'acc': Absolute error
         - 'eps': Relative error
     Returns:
         - 'Q': The integral of F from a to b
         - 'err': An estimate of the error on Q
     """
-    assert a < b, 'Check your limits!'
+    assert a_orig < b_orig, 'Check your limits!'
     # An infinite limit integral is converted by a variable transformation
     # to a finite limit integral
-    if np.any(np.isinf([a, b])):
-        if a is np.NINF and b is not np.PINF:
-            F = f 
+    if np.any(np.isinf([a_orig, b_orig])):
+        if a_orig is np.NINF and b_orig is not np.PINF:
+            f = F
+            F = lambda t: f(b + (t / (1 + t))) * (1 / ((1 + t) ** 2))
             a = -1
             b = 0
-            def f(x):
-                return F(b + (t / (1 + t))) * (1 / ((1 + t) ** 2))
-        elif a is not np.NINF and b is np.PINF:
-            F = lambda t: F(a + (t / (1 - t))) * (1 / ((1 - t) ** 2))
+        elif a_orig is not np.NINF and b_orig is np.PINF:
+            f = F
+            F = lambda t: f(a + (t / (1 - t))) * (1 / ((1 - t) ** 2))
             a = 0
             b = 1
         else:
-            F = lambda t: F(t / (1 - t ** 2)) * ((1 + t ** 2) / (1 - t ** 2) ** 2)
+            f = F
+            F = lambda t: f(t / (1 - t ** 2)) * ((1 + t ** 2) / (1 - t ** 2) ** 2)
             a = -1
             b = 1
 
