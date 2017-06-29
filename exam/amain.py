bdbe98d68eec432f3106c0c4f659a97647974878
Amalie Stokholm <stokholm@post.au.dk>
2017-06-29 17:54:15 +0200

    commit

--- exam/amain.py
+++ exam/amain.py
@@ -13,14 +13,26 @@ def amain():
     diag = np.random.rand(n)
     D = np.diag(diag)
     u = np.random.rand(n)
+    D_copy = np.copy(D)
+    u_copy = np.copy(u)
     p = 2
     print(D)
     print(u)
-    update.update(D, u, p)
+    l, eveq = update.update(D, u, p)
 
-    A = np.copy(D)
-    A[p] = u
-    A[:, p] = u
-    A[p, p] = D[p, p] + u[p] ** 2
-    print(np.linalg.eig(A))
+    print(l)
+    print(eveq(l))
+
+    A = np.copy(D_copy)
+    A[p] += u_copy
+    A[:, p] += u_copy
+    test_lambda, test_vectors = np.linalg.eig(A)
+
+    print('Print from numpy')
+    print(test_lambda)
+    print(eveq(test_lambda))
+
+    print('Difference')
+    print(np.sort(test_lambda) - np.sort(l))
+
 amain()
--- exam/update.py
+++ exam/update.py
@@ -4,6 +4,7 @@ import sys
 sys.path.append(os.path.join(os.path.dirname(__file__), '../5_roots/'))
 assert os.path.exists(sys.path[-1]), sys.path[-1]
 import root_finding as roots
+import scipy.optimize
 
 
 def update(D, u, p):
@@ -11,6 +12,7 @@ def update(D, u, p):
     This routine computes the eigenvalues of the matrix A,
     which is given as A = D + e(p*)u^T + u*e(p)^T.
     A is a size-n real symmetric matrix.
+    D and u are modified if u[p] != 0.
     Arguments:
         - 'D': Diagonal matrix of size n x n
         - 'u': Vector
@@ -27,16 +29,17 @@ def update(D, u, p):
     assert np.logical_and(1 <= p, p <= n) , 'p is not within the range (1 < p < n)!'
     assert np.allclose(D, D.real), 'D is not real!'
 
-    A = np.zeros((n, n), dtype='float64')
     x0 = np.zeros(n, dtype='float64')
     dx = np.zeros(n, dtype='float64')
+    
+    # Assumtion
+    D[p, p] += 2 * u[p]
+    u[p] = 0 
 
     # Fill
     for i in range(n):
         x0[i] = D[i, i]
-    
     dx = np.copy(u)
-
 
     def eveq(l):
         """
@@ -53,8 +56,6 @@ def update(D, u, p):
                         t *= (D[i, i] - l)
                 f += t 
         return f
-    l = roots.newton(eveq, x0, dx)
-    print(l)
-    for i in range(len(l)):
-        print(eveq(l[i]))
-    return l
+    l = scipy.optimize.fsolve(eveq, x0)
+    # l = roots.newton(eveq, x0, dx)
+    return l, eveq
